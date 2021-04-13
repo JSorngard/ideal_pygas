cfc64d3daf5a09d258acc90d8ea0a37b878be324
Johan Sörngård <johan.sorngard@fysik.su.se>
2021-04-13 13:11:39 +0200

    Removed commented code

--- gas_simulation.py
+++ gas_simulation.py
@@ -117,21 +117,6 @@ def numba_update_positions(xs,ys,pxs,pys,circle_box,box_size,delta_t,edge_collis
 
 	return xs,ys,pxs,pys
 
-# class bcolors:
-# 	"""
-# 	Contains variables that can be used in formatted strings to
-# 	e.g. change the colour of the text.
-# 	"""
-# 	HEADER = '\033[95m'
-# 	OKBLUE = '\033[94m'
-# 	OKCYAN = '\033[96m'
-# 	OKGREEN = '\033[92m'
-# 	WARNING = '\033[93m'
-# 	FAIL = '\033[91m'
-# 	ENDC = '\033[0m'
-# 	BOLD = '\033[1m'
-# 	UNDERLINE = '\033[4m'
-
 if __name__ == '__main__':
 	import matplotlib.pyplot as plt
 	import matplotlib.animation
